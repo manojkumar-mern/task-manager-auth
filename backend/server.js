10324e103c5e6c12587f868ddb356424149ebc99
manojkumar <vijaymanoj0000@gmail.com>
2026-03-10 16:36:19 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,17 @@ connectDB();
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API running ✅");
+  res.send("API running");
 });
 
 // routes
@@ -21,4 +27,4 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(` Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
